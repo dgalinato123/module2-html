c96ba1470a9121b38d005c740953a189a806478a
JomArsF14 <arsula.jomarif@gmail.com>
2026-02-19 11:56:11 +0800

    WSAT-M4: JavaScript interactivity added

---
+++ js/script.js
@@ -0,0 +1,32 @@
+const submitBtn = document.getElementById("submitBtn");
+submitBtn.addEventListener("click", function(event) {
+    event.preventDefault();
+    alert("Thank you! Your message has been received.");
+});
+
+const themeToggle = document.getElementById("theme-toggle");
+themeToggle.addEventListener("click", function() {
+    document.body.classList.toggle("dark-mode");
+});
+
+const toggleSkills = document.getElementById("toggleSkills");
+const skillsSection = document.getElementById("skillsSection");
+
+toggleSkills.addEventListener("click", function() {
+    skillsSection.classList.toggle("hidden");
+    if (skillsSection.classList.contains("hidden")) {
+        toggleSkills.textContent = "Show Skills";
+    } else {
+        toggleSkills.textContent = "Hide Skills";
+    }
+});
+submitBtn.addEventListener("click",function(event){
+    event.preventDefault();
+    const name = document.getElementById("name").value;
+    const email = document.getElementById("email").value;
+    if (name===""||email===""){
+    alert("Please fill in all required fields.");
+    } else{
+    alert("Form submitted successfully!");
+    }
+    });
