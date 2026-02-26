6a20ccac85130b5b51c87904af159905f3b59938
dgalinato123 <dgalinato123@gmail.com>
2026-02-26 21:51:08 +0800

    WSAT-M5: Migrated CV to React

--- personal-cv-react/vite.config.js
+++ personal-cv-react/vite.config.js
@@ -1,8 +1,7 @@
 import { defineConfig } from 'vite'
-import react from '@vitejs/react-swc'
+import react from '@vitejs/plugin-react-swc'
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/module2-html/", // 👈 THIS IS THE MAGIC LINE YOU NEED
+  base: "/module2-html/",
 })
