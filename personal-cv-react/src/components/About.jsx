334aeab04ab4273be11191d7127bd9792fb3d02f
dgalinato123 <dgalinato123@gmail.com>
2026-02-26 22:06:12 +0800

    WSAT-M5: Migrated CV to React

--- personal-cv-react/src/components/About.jsx
+++ personal-cv-react/src/components/About.jsx
@@ -1,8 +1,9 @@
+import profile from "../assets/profile.jpg";
 function About() {
     return (
       <section className="card about-me-card">
         <h2>About Me</h2>
-        <img src="./assets/profile.jpg" alt="Profile Picture" width="200" />
+       <img src={profile} alt="Profile Picture" />
         <p>I am 19 years old and a student at the University of Southern Philippines.</p>
         <p>
           Email: <a href="mailto:dgalinato123@gmail.com">dgalinato123@gmail.com</a><br />
